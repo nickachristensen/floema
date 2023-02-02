f4f1abcfcd7464400f70c8b2c3a0b43f5f75754b
Nicholas A Christensen <nickachristensen@gmail.com>
2023-02-01 23:45:11 -0500

    finished webpack

--- webpack.config.js
+++ webpack.config.js
@@ -104,21 +104,22 @@ module.exports = {
                 use: [
                     {
                     loader: ImageMinimizerPlugin.loader,
-                    options: {
-                        severityError: 'warning',
-                        minimizerOptions: {
-                          plugins: [
-                            "imagemin-gifsicle",
-                            "imagemin-mozjpeg",
-                            "imagemin-pngquant",
-                            "imagemin-svgo",
-                                ],
-                            },
-                        },
                     },
-                
                 ],
             },
+
+            {
+                test: /\.(glsl|frag|vert)$/,
+                loader: 'raw-loader',
+                exclude: /node_modules/
+            },
+
+            {
+                test: /\.(glsl|frag|vert)$/,
+                loader: 'glslify-loader',
+                exclude: /node_modules/
+            }
+
         ]
     }
 }

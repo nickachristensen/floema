3edbe80f308d54b3856a480a9c9bd192668b661c
Nicholas A Christensen <nickachristensen@gmail.com>
2023-06-26 19:40:23 -0400

    Add use of Link throught out pug files add navigation markdown and add link validations in link resolver

--- app.js
+++ app.js
@@ -28,26 +28,21 @@ const initApi = req => {
 }
 
 const handleLinkResolver = doc => {
-  // // Define the url depending on the document type
+  if (doc.type === 'product') {
+    return `detail/${doc.slug}`
+  }
 
-  // if (doc.type === 'page') {
-  //   return '/page/' + doc.uid
-  // } else if (doc.type === 'blog_post') {
-  //   return '/blog/' + doc.uid
-  // }
+  if (doc.type === 'about') {
+    return '/about'
+  }
 
-  // // Default to homepage
-
-  // return '/'
+  if (doc.type === 'collections') {
+    return '/collections'
+  }
 }
 
 app.use((req, res, next) => {
-  // res.locals.ctx = {
-  //   endpoint: process.env.PRISMIC_ENDPOINT,
-  //   linkResolver: handleLinkResolver
-  // }
-
-  res.locals.Links = handleLinkResolver
+  res.locals.Link = handleLinkResolver
 
   res.locals.Numbers = index => {
     return index === 0 ? 'One' : index === 1 ? 'Two' : index === 2 ? 'Three' : index === 3 ? 'Four' : ''
@@ -61,58 +56,63 @@ app.use((req, res, next) => {
 app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'pug')
 
+const handleRequest = async api => {
+  const meta = await api.getSingle('meta')
+  const navigation = await api.getSingle('navigation')
+  const preloader = await api.getSingle('preloader')
+
+  return {
+    meta,
+    navigation,
+    preloader
+  }
+}
+
 app.get('/', async (req, res) => {
   const api = await initApi(req)
-  const meta = await api.getSingle('meta')
   const home = await api.getSingle('home')
-  const preloader = await api.getSingle('preloader')
+  const defaults = await handleRequest(api)
 
   const { results: collections } = await api.query(Prismic.Predicates.at('document.type', 'collection'), {
     fetchLinks: 'product.image'
   })
 
   res.render('pages/home', {
+    ...defaults,
     collections,
-    home,
-    meta,
-    preloader
+    home
   })
 })
 
 app.get('/about', async (req, res) => {
   const api = await initApi(req)
-  const meta = await api.getSingle('meta')
   const about = await api.getSingle('about')
-  const preloader = await api.getSingle('preloader')
+  const defaults = await handleRequest(api)
 
   res.render('pages/about', {
-    about,
-    meta,
-    preloader
+    ...defaults,
+    about
   })
 })
 
 app.get('/detail/:uid', async (req, res) => {
   const api = await initApi(req)
-  const meta = await api.getSingle('meta')
-  const preloader = await api.getSingle('preloader')
+  const defaults = await handleRequest(api)
 
   const product = await api.getByUID('product', req.params.uid, {
     fetchLinks: 'collection.title'
   })
 
   res.render('pages/detail', {
-    meta,
-    preloader,
+    ...defaults,
     product
   })
 })
 
 app.get('/collections', async (req, res) => {
   const api = await initApi(req)
-  const meta = await api.getSingle('meta')
+  const defaults = await handleRequest(api)
   const home = await api.getSingle('home')
-  const preloader = await api.getSingle('preloader')
 
   const { results: collections } = await api.query(Prismic.Predicates.at('document.type', 'collection'), {
     fetchLinks: 'product.image'
@@ -121,10 +121,9 @@ app.get('/collections', async (req, res) => {
   console.log(collections)
 
   res.render('pages/collections', {
+    ...defaults,
     collections,
-    home,
-    meta,
-    preloader
+    home
   })
 })
 

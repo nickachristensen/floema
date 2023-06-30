39a4936117ff9e83b15953d09b0890170b3a52a8
Nicholas A Christensen <nickachristensen@gmail.com>
2023-06-29 22:03:45 -0400

    Add foolder structure for javascript in app.  Add logic to index.js and linked to About

---
+++ app/index.js
@@ -0,0 +1,18 @@
+import About from 'pages/About'
+
+class App {
+  constructor() {
+    console.log('App')
+  }
+
+  createPages() {
+    this.pages = {
+      about: new About(),
+      collections: new Collections(),
+      detail: new Detail(),
+      home: new Home()
+    }
+  }
+}
+
+new App()

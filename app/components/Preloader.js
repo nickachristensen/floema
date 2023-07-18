38d1386ead477e0b97ec14d23b96e794fe0119a7
Nicholas Christensen <nickachristensen@gmail.com>
2023-07-17 21:55:02 -0400

    Add loading logic to Preloader.js

--- app/components/Preloader.js
+++ app/components/Preloader.js
@@ -13,15 +13,26 @@ export default class Preloader extends Component {
       }
     })
 
+    this.length = 0
+
     console.log(this.element, this.elements)
 
     this.createLoader()
   }
 
   createLoader () {
-    each(this.elements.images, image => {
-      
-      console.log(image)
+    each(this.elements.images, element => {
+      const image = new Image()
+
+      image.onload = () => this.onAssetLoaded(image)
+      image.src = element.getAttribute('data-src')
+
     })
+  }
+
+  onAssetLoaded (image) {
+    this.length += 1
+
+    console.log(Math.round(this.length / this.elements.images.length * 100))
   }
 }

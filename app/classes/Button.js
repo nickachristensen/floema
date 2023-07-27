c45175a26692050c786a7d854d6d5b2e118d3244
Nicholas Christensen <nickachristensen@gmail.com>
2023-07-27 11:41:52 -0400

    Add new svg button to home and detail pug and created Button class

---
+++ app/classes/Button.js
@@ -0,0 +1,15 @@
+import Component from './Component'
+
+export default class Button extends Component {
+    constructor ({ element }) {
+        super({ element })
+    }
+
+    addEventListeners () {
+
+    }
+
+    removeEventListeners () {
+        
+    }
+}

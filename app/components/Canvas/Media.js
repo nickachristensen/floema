d33d2725240f1b6a16eafb919597de940f4fc4f7
Nicholas Christensen <nickachristensen@gmail.com>
2023-08-07 21:59:59 -0400

    add scroll update and change where height and width are coming from

--- app/components/Canvas/Media.js
+++ app/components/Canvas/Media.js
@@ -48,6 +48,7 @@ export default class {
     }
 
     createBounds ({ sizes }) {
+        this.sizes = sizes
         this.bounds = this.element.getBoundingClientRect()
 
         this.updateScale(sizes)
@@ -55,23 +56,28 @@ export default class {
         this.updateY()
     }
 
-    updateScale ({ height, width }) {
+    updateScale () {
         this.height = this.bounds.height / window.innerHeight
         this.width = this.bounds.width / window.innerWidth
 
-        this.mesh.scale.x = width * this.width
-        this.mesh.scale.y = height * this.height
+        this.mesh.scale.x = this.sizes.width * this.width
+        this.mesh.scale.y = this.sizes.height * this.height
 
         this.x = this.bounds.left / window.innerWidth
         this.y = this.bounds.top / window.innerHeight
     }
 
     updateX (x = 0) {
-        this.mesh.position.x = (-width / 2) + (this.mesh.scale.x / 2) + (this.x * width)
+        this.mesh.position.x = (-this.sizes.width / 2) + (this.mesh.scale.x / 2) + (this.x * this.sizes.width)
     }
 
     updateY (y = 0) {
-        this.mesh.position.y = (height / 2) - (this.mesh.scale.y / 2) - (this.y * height)
+        this.mesh.position.y = (this.sizes.height / 2) - (this.mesh.scale.y / 2) - (this.y * this.sizes.height)
+    }
+
+    update (scroll) {
+        this.updateX(scroll.x)
+        this.updateY(scroll.y)
     }
 
     onResize (sizes) {

9bb0115b2eb78a498fdf6d1d50ff4d2f44144853
Nicholas Christensen <nickachristensen@gmail.com>
2023-11-14 22:23:42 -0500

    Add rotation and opacity modifiers on details medias

--- app/components/Canvas/Collections/Media.js
+++ app/components/Canvas/Collections/Media.js
@@ -117,11 +117,18 @@ export default class {
         this.mesh.position.y = (this.sizes.height / 2) - (this.mesh.scale.y / 2) - (this.y * this.sizes.height) + this.extra.y
     }
 
-    update (scroll) {
-        
+    update (scroll, index) {
         this.updateX(scroll)
-        this.updateY()
 
-        this.programe.uniforms.uAlpha.value = this.opacity.multiplier
+        const amplitude = 0.1
+        const frequency = 1
+
+        this.mesh.rotation.z = -0.02 * Math.PI * Math.sin(this.index / frequency)
+        this.mesh.position.y = amplitude * Math.sin(this.index / frequency)
+
+        this.opacity.target = index === this.index ? 1 : 0.4
+        this.opacity.current = GSAP.utils.interpolate(this.opacity.current, this.opacity.target, this.opacity.lerp)
+
+        this.program.uniforms.uAlpha.value = this.opacity.multiplier * this.opacity.current
     }
 }
